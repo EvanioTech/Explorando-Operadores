d5a33a5095389d8cb87192312558c12dc63ac4d5
Evanio Tech <evanioshark@gmail.com>
2026-04-09 07:05:25 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,16 @@
+let user1 = "Matias"
+let age1 = 31
+let sexo1 = "Masculino"
+
+let user2 = "Lucia"
+let age2 = 28
+let sexo2 = "Femenino"
+
+let user3 = "Sofia"
+let age3 = 25
+let sexo3 = "Femenino"
+
+console.log("Usuario 1: " + user1 + ", Idade: " + age1 + ", Sexo: " + sexo1)
+console.log("Usuario 2: " + user2 + ", Idade: " + age2 + ", Sexo: " + sexo2)
+console.log("Usuario 3: " + user3 + ", Idade: " + age3 + ", Sexo: " + sexo3) 
+
