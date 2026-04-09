34627d16a1344bcebc15b5a696cb17a4fc0b9047
Evanio Tech <evanioshark@gmail.com>
2026-04-09 07:15:50 -0300

    Usando operador de Comparação

--- index.js
+++ index.js
@@ -10,10 +10,26 @@ let user3 = "Sofia"
 let age3 = 25
 let sexo3 = "Femenino"
 
+let user4 = "Pedro"
+let age4 = 17
+let sexo4 = "Masculino"
+
 console.log("Usuario 1: " + user1 + ", Idade: " + age1 + ", Sexo: " + sexo1)
 console.log("Usuario 2: " + user2 + ", Idade: " + age2 + ", Sexo: " + sexo2)
-console.log("Usuario 3: " + user3 + ", Idade: " + age3 + ", Sexo: " + sexo3) 
+console.log("Usuario 3: " + user3 + ", Idade: " + age3 + ", Sexo: " + sexo3)
+console.log("Usuario 4: " + user4 + ", Idade: " + age4 + ", Sexo: " + sexo4)
 
 let frequentaFestaNova = age1 >= 18
 
 console.log("O usuário " + user1 + " frequenta a festa nova? " + frequentaFestaNova)
+console.log("O usuário " + user2 + " frequenta a festa nova? " + (age2 >= 18))
+console.log("O usuário " + user3 + " frequenta a festa nova? " + (age3 >= 18))
+console.log("O usuário " + user4 + " frequenta a festa nova? " + (age4 >= 18))
+
+let frequentaFestaPink = sexo1 === "Femenino"
+
+console.log("O usuário " + user1 + " frequenta a festa pink? " + frequetaFestaPink)
+console.log("O usuário " + user2 + " frequenta a festa pink? " + (sexo2 === "Femenino"))
+console.log("O usuário " + user3 + " frequenta a festa pink? " + (sexo3 === "Femenino"))
+console.log("O usuário " + user4 + " frequenta a festa pink? " + (sexo4 === "Femenino"))
+
