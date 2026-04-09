b68e26670984b6beb6b469afc70ba9e941b38441
Evanio Tech <evanioshark@gmail.com>
2026-04-09 07:10:48 -0300

    Usando operadores

--- index.js
+++ index.js
@@ -14,3 +14,6 @@ console.log("Usuario 1: " + user1 + ", Idade: " + age1 + ", Sexo: " + sexo1)
 console.log("Usuario 2: " + user2 + ", Idade: " + age2 + ", Sexo: " + sexo2)
 console.log("Usuario 3: " + user3 + ", Idade: " + age3 + ", Sexo: " + sexo3) 
 
+let frequentaFestaNova = age1 >= 18
+
+console.log("O usuário " + user1 + " frequenta a festa nova? " + frequentaFestaNova)
